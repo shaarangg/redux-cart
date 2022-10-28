3e477b2428c2cf9b64b9604cab6a06b088396be8
shaarangg <shaarangg.singh@gmail.com>
2022-10-28 21:29:07 +0530

    feat: redux action

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
+import { createStore } from "redux";
 // redux stuff
 // store/state is used to sore all the state data
 // reducer is used to update the store
@@ -12,6 +13,14 @@ import cartItems from "./cart-items";
 function App() {
     // cart setup
 
+    const initalStore = { count: 1 };
+    function reducer(state, action) {
+        console.log(state);
+        console.log(action);
+        return state;
+    }
+    const store = createStore(reducer, initalStore);
+    store.dispatch({ type: "HELLO" });
     return (
         <main>
             <Navbar />
