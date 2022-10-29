7c3239d8c324ba94f86b3f03892d31a069b2caa6
shaarangg <shaarangg.singh@gmail.com>
2022-10-29 15:29:49 +0530

    feat: INCREASE and DECREASE action

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-import { REMOVE, CLEAR_CART } from "./actions";
+import { REMOVE, CLEAR_CART, INCREASE, DECREASE } from "./actions";
 function reducer(state, action) {
     if (action.type === REMOVE) {
         const id = action.payload.id;
@@ -6,6 +6,33 @@ function reducer(state, action) {
     }
     if (action.type === CLEAR_CART) {
         return { ...state, cart: [] };
+    }
+    if (action.type === INCREASE) {
+        return {
+            ...state,
+            cart: state.cart.map((item) => {
+                if (item.id === action.payload.id) {
+                    item.amount += 1;
+                }
+                return item;
+            }),
+        };
+    }
+    if (action.type === DECREASE) {
+        if (action.payload.amount === 1) {
+            const id = action.payload.id;
+            return { ...state, cart: state.cart.filter((item) => item.id !== id) };
+        } else {
+            return {
+                ...state,
+                cart: state.cart.map((item) => {
+                    if (item.id === action.payload.id) {
+                        item.amount -= 1;
+                    }
+                    return item;
+                }),
+            };
+        }
     }
     return state;
 }
